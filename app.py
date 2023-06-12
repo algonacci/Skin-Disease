71710d00aa2fa3e6a7c8f89a97db15653d7c431d
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-06-12 21:05:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 # from PIL import Image
 # import io
@@ -70,5 +71,7 @@ def predict_image():
         print('Error processing image:', e)
         return jsonify({'error': 'Image processing failed'})
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=False,
+            host="0.0.0.0",
+            port=int(os.environ.get("PORT", 8080)))
